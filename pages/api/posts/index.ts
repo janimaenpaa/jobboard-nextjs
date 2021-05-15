b00efc6ede3b4f79b53ae1a1782e2fa9ec9b5a36
janimaenpaa <itsmayd@gmail.com>
2021-05-15 20:24:14 +0300

    Make /api/posts work with req.body

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -5,28 +5,53 @@ import prisma from '../../../libs/prisma';
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export const getPosts = async () => {
+  const posts = await prisma.post.findMany();
+  return posts;
+};
+
+export const createPost = async (req: NextApiRequest) => {
+  const {
+    title,
+    company,
+    description,
+    requiredSkills = [],
+    niceToHaveSkills = [],
+    url,
+    deadline,
+  } = req.body;
+
+  const newPost: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'status'> = {
+    title,
+    company,
+    description,
+    requiredSkills,
+    niceToHaveSkills,
+    url,
+    deadline: deadline && new Date(deadline),
+  };
+
+  try {
+    const post = await prisma.post.create({ data: newPost });
+    return { data: post };
+  } catch (error) {
+    return { error: 'Some properties missing' };
+  }
+};
+
 const handler = nc<NextApiRequest, NextApiResponse>()
   .get(async (req, res) => {
-    const posts = await prisma.post.findMany();
+    const posts = await getPosts();
     res.json(posts);
   })
   .post(async (req, res) => {
-    const newPost: Omit<Post, 'id' | 'createdAt'> = {
-      title: 'Frontend Developer',
-      company: 'Tiili Solutions',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      requiredSkills: ['React'],
-      niceToHaveSkills: ['NodeJS'],
-      url: 'https://reactjs.org',
-      deadline: new Date('2020-06-01'),
-      status: PostStatus.WAITING,
-      updatedAt: new Date(),
-    };
+    const newPost = await createPost(req);
 
-    const post = await prisma.post.create({ data: newPost });
+    if (newPost.error) {
+      return res.status(400).json(newPost);
+    }
 
-    res.status(201).json({ data: post });
+    res.status(201).json(newPost);
   });
 
 export default handler;
