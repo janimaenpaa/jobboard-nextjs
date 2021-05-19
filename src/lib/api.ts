d63b0216e9824fc2ba87f84957d2ddd569d5b4b0
janimaenpaa <itsmayd@gmail.com>
2021-05-20 00:34:52 +0300

    Fix HTTP requests

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,36 +1,36 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const GET = async (path: string) => {
-  const response = await fetch(`/${API_URL}/${path}`);
+  const response = await fetch(`api/${path}`);
 
-  return response.json();
+  return await response.json();
 };
 
 export const POST = async (path: string, data: object) => {
-  const response = await fetch(`/${API_URL}/${path}`, {
+  const response = await fetch(`api/${path}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
 
-  return response.json();
+  return await response.json();
 };
 
 export const PUT = async (path: string, data: object) => {
-  const response = await fetch(`/${API_URL}/${path}`, {
+  const response = await fetch(`api/${path}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
 
-  return response.json();
+  return await response.json();
 };
 
 export const DELETE = async (path: string) => {
-  const response = await fetch(`/${API_URL}/${path}`, {
-    method: 'PUT',
+  const response = await fetch(`api/${path}`, {
+    method: 'DELETE',
     headers: { 'Content-Type': 'application/json' },
   });
 
-  return response.json();
+  return await response.json();
 };
